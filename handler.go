7e9591202831821395bb748886dc3f47059849d6
Artem Yadelskyi <mymmrac@gmail.com>
2022-08-01 13:23:51 +0300

    Cleaned handler

--- handler.go
+++ handler.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
 	tu "github.com/mymmrac/telego/telegoutil"
@@ -27,52 +29,13 @@ func NewHandler(cfg *config.Config, log logger.Logger, bh *th.BotHandler) *Handl
 
 // RegisterHandlers registers all handlers in bot handler
 func (h *Handler) RegisterHandlers() {
-	h.bh.HandleMessage(h.createInvoice, th.CommandEqual("invoice"))
-	h.bh.HandlePreCheckoutQuery(h.preCheckout)
-	h.bh.HandleMessage(h.successPayment, th.SuccessPayment())
+	h.bh.HandleMessage(h.startCmd, th.CommandEqual("start"))
 }
 
-const currency = "UAH"
-
-func (h *Handler) createInvoice(bot *telego.Bot, message telego.Message) {
+func (h *Handler) startCmd(bot *telego.Bot, message telego.Message) {
 	chatID := message.Chat.ID
-	_, err := bot.SendInvoice(tu.Invoice(
-		tu.ID(chatID),
-		"Test Invoice",
-		"Hello World!",
-		"test-invoice",
-		h.cfg.Settings.ProviderToken,
-		currency,
-		tu.LabeledPrice("🍱 Bento", 2000),
-		tu.LabeledPrice("🍣 Sushi", 1000),
-		tu.LabeledPrice("🚚 Delivery", 0),
-		tu.LabeledPrice("🎟 Coupon", -1500),
-	).
-		WithNeedPhoneNumber().
-		WithNeedShippingAddress().
-		WithMaxTipAmount(5000).
-		WithSuggestedTipAmounts(1000, 3000, 5000))
+	_, err := bot.SendMessage(tu.Message(tu.ID(chatID), fmt.Sprintf("Hello %s", message.From.FirstName)))
 	if err != nil {
-		h.log.Error("Send invoice: ", err)
-	}
-}
-
-func (h *Handler) preCheckout(bot *telego.Bot, query telego.PreCheckoutQuery) {
-	err := bot.AnswerPreCheckoutQuery(tu.PreCheckoutQuery(query.ID, true))
-	if err != nil {
-		h.log.Error("Answer pre checkout: ", err)
-	}
-}
-
-func (h *Handler) successPayment(bot *telego.Bot, message telego.Message) {
-	chatID := message.Chat.ID
-	payment := message.SuccessfulPayment
-
-	_, err := bot.SendMessage(tu.MessageWithEntities(tu.ID(chatID),
-		tu.Entity("Thanks for ordering, shipping to: "),
-		tu.Entity(payment.OrderInfo.ShippingAddress.StreetLine1).Bold(),
-	))
-	if err != nil {
-		h.log.Error("Send message: ", err)
+		h.log.Errorf("Send message: %s", err)
 	}
 }
