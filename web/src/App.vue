ab816e1f6360c7e0a2716a8674fbc8092e57c668
Artem Yadelskyi <mymmrac@gmail.com>
2022-08-03 17:28:44 +0300

    Added demo of Telegram WebApp usage

--- web/src/App.vue
+++ web/src/App.vue
@@ -10,8 +10,40 @@
   <div class="bg-tg-button">tg-button</div>
   <div class="bg-tg-button-text">tg-button-text</div>
   <div class="bg-tg-secondary-bg">tg-secondary-bg</div>
+
+  <button class="m-2 p-2 bg-tg-button text-tg-text rounded">Test {{ count }}</button>
+
+  <br>
+  <hr>
+  <pre class="bg-tg-bg text-tg-text">{{ tgJson }}</pre>
+  <hr>
+  <br>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+
+const tg = window.Telegram.WebApp
+const tgJson = ref(JSON.stringify(tg, null, 4))
+
+function updateTg() {
+  tgJson.value = JSON.stringify(window.Telegram.WebApp, null, 4)
+}
+
+tg.onEvent("themeChanged", updateTg)
+tg.onEvent("viewportChanged", updateTg)
+
+tg.MainButton.setText("+")
+tg.MainButton.show()
+
+const count = ref(0)
+tg.onEvent("mainButtonClicked", () => {
+  count.value++
+
+  if (count.value > 3) {
+    tg.MainButton.showProgress(false)
+  }
+})
+</script>
 
 <style scoped lang="scss"></style>
